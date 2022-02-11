0a71e18dbfe0e3b2e15cb4ab29a995cdcb3ca26b
Chandan Chirag <93903887+chandanchirag11812444@users.noreply.github.com>
2022-02-11 12:58:42 +0530

    Add files via upload

---
+++ Recursion/Parameterised and Functional Recursion/pr02.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int fun(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return n + fun(n - 1);
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+    cout << fun(n) << endl;
+    return 0;
+}
